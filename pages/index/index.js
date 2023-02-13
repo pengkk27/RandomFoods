be6e5805ca08979457aba275b1b5511f0c3d81c5
pengkk27 <1481128018@qq.com>
2023-02-13 20:48:50 +0800

    编写提示文字动画，和按钮反应

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,15 +7,39 @@ var defaultFoods = ["牛肉面", "快餐", "麻辣烫", "包子", "饺子", "馄
 
 Page({
   data: {
+    animationData: {},
+    foodAnimation: {},
+    tipText: '这顿吃啥',
+    stop: false,
+    food: 'kljlk'
   },
 
-  // 初始参数
-  startRandom: function() {
-    var random = Math.round(Math.random() * (defaultFoods.length - 1));
-    wx.redirectTo({
-      url: '/pages/result/result?random=' + random,
+  // 开始随机按钮进行的操作
+  handleClick: function() {
+    this.animationData = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'linear'
     })
+    this.foodAnimation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'linear'
+    })
+    this.animationData.translate3d(-75, -60, 0).scale(0.6).step()
+    this.foodAnimation.translate3d(0, -110, 0).opacity(1).step()
+    this.setData({
+      animationData: this.animationData.export(),
+      foodAnimation: this.foodAnimation.export() 
+    })
+    
+    var random = Math.round(Math.random() * (defaultFoods.length - 1));
+    this.setData({
+      food: defaultFoods[random]
+    })
+    
   },
+
+
+
 
   onLoad() {
   }
